4fb1af8387d861373a62a0f2874e28fe0dc0dcac
An-Ran Lee <alee208@ucsc.edu>
2024-05-18 20:27:24 -0700

    add dropdown

--- PeanutEval/islands/CodeBox.tsx
+++ PeanutEval/islands/CodeBox.tsx
@@ -1,15 +1,29 @@
-import { h } from "preact";
-
+import modelList from "../utils/model_list.ts";
+import { Signal } from "@preact/signals";
 interface CodeBoxProps {
-  code: string;
+  code: Signal<number>;
 }
 
 export default function CodeBox({ code }: CodeBoxProps) {
+  const handleDropdownChange = (event) => {
+    code.value = modelList.indexOf(event.target!.value);
+  };
   return (
-    <div class="p-4 bg-gray-100 rounded-lg shadow-md">
-      <pre class="whitespace-pre-wrap">
-        <code>{code}</code>
-      </pre>
-    </div>
+    <>
+      <div class="flex flex-col">
+        <select value={code} onChange={handleDropdownChange}>
+          {modelList.map((model, index) => (
+            <option key={index} value={index}>
+              {model}
+            </option>
+          ))}
+        </select>
+      <div class="p-4 bg-gray-100 rounded-lg shadow-md">
+        <pre class="whitespace-pre-wrap">
+        <code></code>
+        </pre>
+      </div>
+      </div>
+    </>
   );
 }
--- PeanutEval/routes/index.tsx
+++ PeanutEval/routes/index.tsx
@@ -2,12 +2,13 @@ import { useSignal } from "@preact/signals";
 import CodeBox from "../islands/CodeBox.tsx";
 import InputBox from "../islands/Input.tsx";
 export default function Home() {
-  const count = useSignal(3);
+  const dropdown_left = useSignal(0);
+  const dropdown_right = useSignal(0);
   return (
     <div class="px-4 py-8 mx-auto bg-[#10111d]">
       <div class="max-w-screen-md mx-auto flex flex-row items-center justify-between">
-        <CodeBox code={`const count = useSignal(${count})`} />
-        <CodeBox code={`const count = useSignal(${count})`} />
+        <CodeBox code={dropdown_left} />
+        <CodeBox code={dropdown_right} />
       </div>
 
       
