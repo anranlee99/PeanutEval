31424c5eae9e0d632989b60b026cb1c1154de652
Natalie Marks <nmarks413@gmail.com>
2024-05-18 21:57:31 -0700

    update compare

--- PeanutEval/routes/api/compare.ts
+++ PeanutEval/routes/api/compare.ts
@@ -2,15 +2,24 @@ import { Handlers } from "$fresh/server.ts";
 import {sendPrompt} from "../../utils/runpod.ts";
 
 export const handler: Handlers = {
-  async POST(request: Request) {
+  async POST(request: Request): Promise<Response> {
     // Parse the request body as FormData
     const formData = await request.formData();
     
-    const model_left = formData.get("model_left");
-    const model_right = formData.get("model_right");
-    console.log("hi!");
+    const model_left = formData.get("model_left") as string;
+    const model_right = formData.get("model_right") as string;
     console.log(model_left, model_right);
-    //TODO: nma?
+
+    const resp = await getCurrentServerlessInstances();
+
+    resp.data.myself.endpoints.forEach(dict => {
+        if (!dict.name.includes(model_left)){
+          sendPrompt("TODO", model_left);
+        }
+        if (model_left != model_right && !dict.name.includes(model_right)){
+          sendPrompt("TODO", model_right);
+        }
+    });
     // check if we have a serverless instance running here
     // if yes, then query the prompt 
     // if not, make an api call to /api/create to create a new serverless instance
@@ -42,7 +51,7 @@ export const handler: Handlers = {
 };
 
 async function getCurrentServerlessInstances(){
-  const response: Response = await fetch("https://api.runpod.io/graphql" + Deno.env.get("RUNPOD_API_KEY"), {
+  const response: Response = await fetch("https://api.runpod.io/graphql?api_key=" + Deno.env.get("RUNPOD_API_KEY"), {
     "headers": {
         "content-type": "application/json",
     },
@@ -50,5 +59,5 @@ async function getCurrentServerlessInstances(){
     "body": "{\"operationName\":\"getEndpoints\",\"variables\":{},\"query\":\"query getEndpoints {\\n  myself {\\n    id\\n    serverlessDiscount {\\n      discountFactor\\n      type\\n      expirationDate\\n      __typename\\n    }\\n    endpoints {\\n      aiKey\\n      gpuIds\\n      allowedCudaVersions\\n      id\\n      idleTimeout\\n      locations\\n      name\\n      networkVolumeId\\n      pods {\\n        desiredStatus\\n        __typename\\n      }\\n      scalerType\\n      scalerValue\\n      templateId\\n      userId\\n      workersMax\\n      workersMin\\n      gpuCount\\n      instanceIds\\n      computeType\\n      template {\\n        containerDiskInGb\\n        containerRegistryAuthId\\n        dockerArgs\\n        env {\\n          key\\n          value\\n          __typename\\n        }\\n        imageName\\n        boundEndpointId\\n        __typename\\n      }\\n      executionTimeoutMs\\n      __typename\\n    }\\n    __typename\\n  }\\n}\"}",
     "method": "POST",
 });  
-  console.log(response);
+  return response.json();
 }
--- PeanutEval/utils/runpod.ts
+++ PeanutEval/utils/runpod.ts
@@ -1,19 +1,18 @@
 import OpenAI from "https://deno.land/x/openai@v4.47.1/mod.ts";
 
-export async function sendPrompt(prompt: string) {
+export async function sendPrompt(prompt: string, model: string) {
   const RUNPOD_API_KEY = Deno.env.get("RUNPOD_API_KEY") as string;
-  const ENDPOINT_ID = Deno.env.get("RUN_ENDPOINT_ID") as string;
 
   const client = new OpenAI({
       apiKey: RUNPOD_API_KEY,
-      baseURL: `https://api.runpod.ai/v2/${ENDPOINT_ID}/openai/v1`,
+      baseURL: `https://api.runpod.ai/v2/${model}/openai/v1`,
     });
 
   const completion = await client.chat.completions.create({
-    model: "meta-llama/Meta-Llama-3-8B-Instruct" ,
+    model: model,
     messages: [{
       "role": "user",
-      "content": "Is Chipotle worth the price?",
+      "content": prompt,
     }],
     temperature: 0,
     max_tokens: 100,
